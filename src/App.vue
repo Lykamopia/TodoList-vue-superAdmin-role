8514a5494ddc70009c84d71ebfb1acfc30a21545
Lykamopia <www.alex94lykam@gmail.com>
2023-06-17 14:19:02 +0300

    external links

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
 <template>
   <!-- A place where all components get swapped -->
   <router-view />
-  <div class="flex text-black justify-evenly items-center mt-4 bg-gray-200 shadow-md">
-    <a href="https://github.com/Lykamopia/TodoList" target="_blank" class="mdi mdi-github text-4xl"></a>
-    <a href="https://www.figma.com/file/gOAVRri0br5yt8mnEUP4bA/To-do-List?type=design&node-id=0%3A1&t=MeXZ0S4efz2DjpuJ-1" target="_blank">    
-      <img src="./assets//figma-icon.png" alt="Figma" class="w-6 h-8 cursor-pointer">
+  <div class="fixed text-black  shadow-md h-24 top-64">
+    <a href="https://github.com/Lykamopia/TodoList" target="_blank" class="mdi mdi-github text-4xl m-3"></a>
+    <a href="https://www.figma.com/file/gOAVRri0br5yt8mnEUP4bA/To-do-List?type=design&node-id=0%3A1&t=MeXZ0S4efz2DjpuJ-1" target="_blank" class="m-4">    
+      <img src="./assets//figma-icon.png" alt="Figma" class="w-6 h-8 cursor-pointer m-3">
     </a>
   </div>
 </template>

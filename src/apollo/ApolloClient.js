e4f033bf50b022625f6d629b88ff673c2340b8f5
Lykamopia <www.alex94lykam@gmail.com>
2023-06-23 09:35:34 +0300

    local .env setup

--- src/apollo/ApolloClient.js
+++ src/apollo/ApolloClient.js
@@ -4,10 +4,9 @@ const httpLink = createHttpLink({
  
   uri: 'https://coherent-reindeer-44.hasura.app/v1/graphql',
   headers:{
-    "X-Hasura-Admin-Secret":import.meta.env.VITE_admin_secret
+    "X-Hasura-Admin-Secret":import.meta.env.VITE_ADMIN_SECRET
   }
 })
-
 const cache = new InMemoryCache()
 
 const apolloClient = new ApolloClient({

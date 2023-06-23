1101159b27363c3048604b679c971f1b6cf4c2b9
Lykamopia <www.alex94lykam@gmail.com>
2023-06-23 09:13:25 +0300

    local .env setup

--- src/apollo/ApolloClient.js
+++ src/apollo/ApolloClient.js
@@ -4,7 +4,7 @@ const httpLink = createHttpLink({
  
   uri: 'https://coherent-reindeer-44.hasura.app/v1/graphql',
   headers:{
-    "X-Hasura-Admin-Secret":"mMMkqdSuROhkicdhf9VPLijs0xXRa3otsyo2JgGdi6aY1e6hwYGGQYCmqyMc08aC"
+    "X-Hasura-Admin-Secret":import.meta.env.VITE_admin_secret
   }
 })
 
